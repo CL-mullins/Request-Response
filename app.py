abb1f9f39b5bb0e7fd65b2fb2b2dc84d739b017d
Christopher Mullins <cmullins318@gmail.com>
2021-02-15 11:29:53 -0800

    Added more routes (not finished)

--- app.py
+++ app.py
@@ -1,3 +1,31 @@
+from flask import flask
 
-# TODO: Follow the assignment instructions to complete the required routes!
-# (And make sure to delete this TODO message when you're done!)
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    """Shows a greeting to the user."""
+    return 'Are you there, world? It\'s me, Ducky!'
+
+@app.route('/animal/<users_animal>')
+def favorite_animal(users_animal):
+    """Display a message to the user that changes based on their favorite animal."""
+    return f'Wow, {users_animal} is my favorite animal, too!'
+
+@app.route('/dessert/<users_dessert>')
+def  favorite_dessert(users_dessert):
+    """Displays a message to the user that changes based on their favorite dessert."""
+    return f'Wow, {users_dessert} is my favorite dessert, too!'
+
+@app.route('/madlibs/<adjective>/<noun>')
+def madLibs(adjective, noun):
+    """Displays a funny (but appropriate) string!"""
+    return f"I think this game is {adjective}, but my teacher is {noun}"
+
+@app.route('/multiply/<number1>/<number2>')
+def multiply(number1, number2):
+    """Multiplies number 1 by number 2"""
+    return number1 * number2
+
+if __name__ == '__main__':
+    app.run(debug=True)
